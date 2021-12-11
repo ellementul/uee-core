47246a0f1f4da1beb218d4738acb4fccf6138d6b
ellementul <ananev.ivan.ur@ya.ru>
2021-12-11 16:26:20 +0500

    Added Create event function in events module

--- src/evnets/index.spec.ts
+++ src/evnets/index.spec.ts
@@ -1,5 +1,64 @@
+jest.mock('crypto', () => {
+  const originalModule = jest.requireActual('crypto')
+
+  //Mock the default export and named export 'foo'
+  return {
+    __esModule: true,
+    ...originalModule,
+    randomUUID: jest.fn(originalModule.randomUUID)
+  };
+});
+
+import { randomUUID } from 'crypto'
 import Events from './index'
 
-test('', () => {
-  expect(2).toBe(2)
+
+
+test('Contsructor', () => {
+  const events = new Events()
+  expect(events).toBeInstanceOf(Events)
+})
+
+describe('Create event', () => {
+  const events = new Events()
+  const name = 'Something Name!'
+
+  test('New event', () => {
+    
+    const cbUpdateEvents = jest.fn((eventList) => {
+      expect(eventList).toEqual([{ name, uid: expect.any(String) }])
+    })
+
+    events.onUpdateEvents(cbUpdateEvents)
+    events.createEvent(name)
+
+    expect(cbUpdateEvents).toHaveBeenCalledTimes(1)
+  })
+
+  test('Repeat event', () => {
+
+    const cbUpdateEvents = jest.fn((eventList) => {
+      expect(eventList).toEqual([{ name, uid: expect.any(String) }])
+    })
+
+    events.onUpdateEvents(cbUpdateEvents)
+    events.createEvent(name)
+
+    expect(cbUpdateEvents).toHaveBeenCalledTimes(0)
+  })
+})
+
+describe('Add listener node', () => {
+  const events = new Events()
+  const nameEvent = 'Something Name!'
+  const nodeUid = randomUUID()
+  events.createEvent(nameEvent)
+
+  test('New listener', () => {
+    events.addListenerNode(nameEvent, nodeUid)
+  })
+
+  test('Repeat listener', () => {
+    events.addListenerNode(nameEvent, nodeUid)
+  })
 })
--- src/evnets/index.ts
+++ src/evnets/index.ts
@@ -1,3 +1,47 @@
-export default class Events {
+import EventEmitter from 'events'
+import { randomUUID } from 'crypto'
 
+interface IEvent {
+  name: string
+  uid: string
 }
+
+class Event implements IEvent {
+  name: string
+  uid: string
+  nodes: string[]
+
+  constructor(name:string) {
+    this.name = name
+    this.uid = randomUUID()
+    this.nodes = []
+  }
+}
+
+type handlerUpdateEventsType = (events: IEvent[]) => void
+
+const updateEvent = Symbol()
+
+class Events {
+  private eventList: Event[] = []
+
+  private emitter = new EventEmitter
+
+  public createEvent(name:string): void {
+    if(this.eventList.some(event => event.name === name))
+      return
+
+    this.eventList.push(new Event(name))
+    this.emitter.emit(updateEvent, this.eventList.map(({ name, uid }) => ({ name, uid })))
+  }
+
+  public onUpdateEvents(cb:handlerUpdateEventsType): void {
+    this.emitter.addListener(updateEvent, cb)
+  }
+
+  public addListenerNode(eventUid, nodeUid): void {
+    this.eventList.filter(event => event.uid === eventUid)
+  }
+}
+
+export default Events
