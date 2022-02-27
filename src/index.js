46be678e55943d06f627828271a6d96296ee38dc
ellementul <ananev.ivan.ur@ya.ru>
2022-02-27 14:29:44 +0500

    Added fabric

--- src/index.js
+++ src/index.js
@@ -1,6 +1,30 @@
 import Dispatcher from "./UEEDispatcher/index.js";
-import Module from "../UEEModule/index.js"
-import Manager from "../UEEManager/index.js"
+import Module from "./UEEModule/index.js"
+import Manager from "./UEEManager/index.js"
 import AbstractTransport from "./UEETransport/abstract-class.js";
 
-export { AbstractTransport, Dispatcher, Module, Manager }
+class UEEFabric {
+  constructor ({ transport } = {}) {
+    const dispatcher = new Dispatcher()
+
+    if (transport) {
+      if(transport instanceof AbstractTransport)
+        dispatcher.connectServer(transport)
+      else
+        throw new Error("The transport doesn't extend AbstractTransport")
+    }
+
+    const manager = new Manager(dispatcher)
+    this.initModule = module => manager.initModule(module)
+    this.run = () => manager.run()
+  }
+
+  async initModules (modules) {
+    const promises =  modules.map(module => {
+      return this.initModule(module)
+    });
+    await Promise.all(promises)
+  } 
+}
+
+export { AbstractTransport,  Module, UEEFabric as UEE }
--- src/tests/index.js
+++ src/tests/index.js
@@ -1,15 +1,10 @@
 import TestModuleOne from "./test-module-one.js"
 import TestModuleTwo from "./test-module-two.js"
 
-import Dispatcher from "../UEEDispatcher/index.js";
-import Manager from "../UEEManager/index.js";
+import { UEE } from "../index.js"
 
 console.log('-----------------TEST--------------------')
 
-const dispatcher = new Dispatcher()
-const manager = new Manager(dispatcher)
-
-//TODO Make to init module or init modules(path to dir of modules)
-await manager.initModule(TestModuleOne)
-await manager.initModule(TestModuleTwo)
-manager.run()
+const envariament = new UEE
+await envariament.initModules([TestModuleOne, TestModuleTwo])
+envariament.run()
