16c59f4bc26b2f7bb1ab50595709ab032c54b9c5
Ellementul <ananev.ivan.ur@ya.ru>
2022-12-18 21:29:19 +0500

    Fixed exports

--- src/index.js
+++ src/index.js
@@ -2,5 +2,11 @@ module.exports = {
   ...require('./Event'),
   ...require('./Provider'),
   ...require('./Member'),
-  ...require('./Transport')
+  ...require('./Transport'),
+  events: {
+    change: require('./Member/events/change_state_event'),
+    connect: require('./Member/events/connected_event'),
+    log: require('./Member/events/log_event'),
+    error: require('./Member/events/error_event')
+  }
 }
